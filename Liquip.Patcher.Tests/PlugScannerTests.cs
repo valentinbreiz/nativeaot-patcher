6a4ccf91026aea9bc079586a71bdc7dff9a0c282
valentinbreiz <valentinbreiz@gmail.com>
2024-12-15 16:49:03 +0100

    ✅ Add tests for LoadPlugs

---
+++ Liquip.Patcher.Tests/PlugScannerTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mono.Cecil;
+using Liquip.API.Attributes;
+using Xunit;
+using Liquip.Patcher;
+
+namespace Liquip.Patcher.Tests
+{
+    public class PlugScannerTests
+    {
+        [Plug(typeof(MockTarget))]
+        public class MockPlug { }
+
+        [Plug("OptionalTarget", IsOptional = true)]
+        public class OptionalPlug { }
+
+        public class MockTarget { }
+
+        public class NonPlug { }
+
+        private AssemblyDefinition CreateMockAssembly<T>()
+        {
+            var assemblyPath = typeof(T).Assembly.Location;
+            return AssemblyDefinition.ReadAssembly(assemblyPath);
+        }
+
+        [Fact]
+        public void LoadPlugs_ShouldFindPluggedClasses()
+        {
+            // Arrange
+            var assembly = CreateMockAssembly<MockPlug>();
+            var scanner = new PlugScanner();
+
+            // Act
+            var plugs = scanner.LoadPlugs(assembly);
+
+            // Assert
+            Assert.Contains(plugs, plug => plug.Name == nameof(MockPlug));
+            var plug = plugs.FirstOrDefault(p => p.Name == nameof(MockPlug));
+            Assert.NotNull(plug);
+
+            var customAttributes = plug.CustomAttributes;
+            Assert.Contains(customAttributes, attr => attr.AttributeType.FullName == typeof(PlugAttribute).FullName);
+            var plugAttribute = customAttributes.First(attr => attr.AttributeType.FullName == typeof(PlugAttribute).FullName);
+
+            var expected = typeof(MockTarget).FullName;
+            var actual = plugAttribute.ConstructorArguments[0].Value?.ToString();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LoadPlugs_ShouldIgnoreClassesWithoutPlugAttribute()
+        {
+            // Arrange
+            var assembly = CreateMockAssembly<NonPlug>();
+            var scanner = new PlugScanner();
+
+            // Act
+            var plugs = scanner.LoadPlugs(assembly);
+
+            // Assert
+            Assert.DoesNotContain(plugs, plug => plug.Name == nameof(NonPlug));
+        }
+
+        [Fact]
+        public void LoadPlugs_ShouldHandleOptionalPlugs()
+        {
+            // Arrange
+            var assembly = CreateMockAssembly<OptionalPlug>();
+            var scanner = new PlugScanner();
+
+            // Act
+            var plugs = scanner.LoadPlugs(assembly);
+            var optionalPlug = plugs.FirstOrDefault(p => p.Name == nameof(OptionalPlug));
+
+            // Assert
+            Assert.NotNull(optionalPlug);
+
+            var customAttributes = optionalPlug.CustomAttributes;
+            Assert.Contains(customAttributes, attr => attr.AttributeType.FullName == typeof(PlugAttribute).FullName);
+            var plugAttribute = customAttributes.First(attr => attr.AttributeType.FullName == typeof(PlugAttribute).FullName);
+            Assert.Equal("OptionalTarget", plugAttribute.ConstructorArguments[0].Value);
+            Assert.True((bool)plugAttribute.Properties.FirstOrDefault(p => p.Name == "IsOptional").Argument.Value);
+        }
+    }
+}
--- Liquip.Patcher/PlugScanner.cs
+++ Liquip.Patcher/PlugScanner.cs
@@ -5,23 +5,27 @@ using MonoMod.Utils;
 
 namespace Liquip.Patcher;
 
-public static class PlugScanner
+public class PlugScanner
 {
 
-    public static List<TypeDefinition> LoadPlugs(params AssemblyDefinition[] assemblies)
+    public List<TypeDefinition> LoadPlugs(params AssemblyDefinition[] assemblies)
     {
         var output = assemblies
-            .SelectMany(assembly =>
-                assembly.Modules
-                    .SelectMany(type => type.Types)
-                    .Where(i=> 
-                        i.HasCustomAttribute(typeof(PlugAttribute).FullName)
-                    )
-            );
-        return output.ToList();
+        .SelectMany(assembly =>
+            assembly.Modules
+                .SelectMany(type => type.Types)
+                .Where(i => i.HasCustomAttribute(typeof(PlugAttribute).FullName))
+        ).ToList();
+
+        foreach (var type in output)
+        {
+            Console.WriteLine($"Plug found: {type.Name}");
+        }
+
+        return output;
     }
 
-    public static List<MethodDefinition> LoadPlugMethods(TypeDefinition plugType)
+    public List<MethodDefinition> LoadPlugMethods(TypeDefinition plugType)
     {
         return plugType.Methods.Where(i => i.IsPublic && i.IsStatic).ToList();
     }
