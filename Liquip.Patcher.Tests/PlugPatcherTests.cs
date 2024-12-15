b33e193388936a26751151c22668e59d8f0bc6d9
valentinbreiz <valentinbreiz@gmail.com>
2024-12-15 22:06:16 +0100

    🐛 Fix patcher + add tests

---
+++ Liquip.Patcher.Tests/PlugPatcherTests.cs
@@ -0,0 +1,48 @@
+﻿using Liquip.API.Attributes;
+using System.Runtime.InteropServices;
+using System;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Liquip.Patcher.Extensions;
+using Xunit;
+using NativeWrapper;
+
+namespace Liquip.Patcher.Tests
+{
+    public class PlugPatcherTests
+    {
+        private AssemblyDefinition CreateMockAssembly<T>()
+        {
+            var assemblyPath = typeof(T).Assembly.Location;
+            return AssemblyDefinition.ReadAssembly(assemblyPath);
+        }
+
+        [Fact]
+        public void PatchType_ShouldReplaceAllMethodsCorrectly()
+        {
+            // Arrange
+            var plugScanner = new PlugScanner();
+            var patcher = new PlugPatcher(plugScanner);
+
+            var assembly = CreateMockAssembly<TestClass>();
+            var targetType = assembly.MainModule.Types.First(t => t.Name == nameof(TestClass));
+
+            var plugAssembly = CreateMockAssembly<TestClassPlug>();
+
+            // Act
+            patcher.PatchType(targetType, plugAssembly);
+
+            // Assert
+            foreach (var plugMethod in plugAssembly.MainModule.Types.First(t => t.Name == nameof(TestClassPlug)).Methods)
+            {
+                if (!plugMethod.IsPublic || !plugMethod.IsStatic) continue;
+
+                var targetMethod = targetType.Methods.FirstOrDefault(m => m.Name == plugMethod.Name);
+                Assert.NotNull(targetMethod);
+                Assert.NotNull(targetMethod.Body);
+                Assert.Equal(plugMethod.Body.Instructions.Count, targetMethod.Body.Instructions.Count);
+            }
+        }
+    }
+}
--- Liquip.Patcher/PlugPatcher.cs
+++ Liquip.Patcher/PlugPatcher.cs
@@ -1,18 +1,104 @@
-using System.Diagnostics.CodeAnalysis;
-using Liquip.Patcher.Extensions;
+using Liquip.API.Attributes;
+using System;
+using System.Linq;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
-using MonoMod.Utils;
+using Liquip.Patcher.Extensions;
 
-namespace Liquip.Patcher;
-
-public static class PlugPatcher
+namespace Liquip.Patcher
 {
-    public static void PatchMethod(MethodDefinition org, MethodDefinition newMethodBody)
+    public class PlugPatcher
     {
-        org.Body = newMethodBody.Body.ReplaceMethodBody(org);
+        private readonly PlugScanner _scanner;
+
+        public PlugPatcher(PlugScanner scanner)
+        {
+            _scanner = scanner ?? throw new ArgumentNullException(nameof(scanner));
+        }
+
+        /// <summary>
+        /// Patches the target method by replacing its body with the body of the plug method.
+        /// </summary>
+        /// <param name="targetMethod">The method in the base class to patch.</param>
+        /// <param name="plugMethod">The plug method providing the replacement body.</param>
+        public void PatchMethod(MethodDefinition targetMethod, MethodDefinition plugMethod)
+        {
+            if (targetMethod == null) throw new ArgumentNullException(nameof(targetMethod));
+            if (plugMethod == null) throw new ArgumentNullException(nameof(plugMethod));
+
+            Console.WriteLine($"Patching method: {targetMethod.FullName} with plug: {plugMethod.FullName}");
+
+            targetMethod.Body = plugMethod.Body.ReplaceMethodBody(targetMethod);
+
+            Console.WriteLine($"Patched method: {targetMethod.Name} successfully.");
+        }
+
+        /// <summary>
+        /// Scans for plug methods and patches corresponding target methods in the target type.
+        /// </summary>
+        /// <param name="targetType">The target class to patch.</param>
+        /// <param name="plugAssemblies">The assemblies to search for plug methods.</param>
+        public void PatchType(TypeDefinition targetType, params AssemblyDefinition[] plugAssemblies)
+        {
+            if (targetType == null) throw new ArgumentNullException(nameof(targetType));
+            if (plugAssemblies == null || plugAssemblies.Length == 0) throw new ArgumentNullException(nameof(plugAssemblies));
+
+            Console.WriteLine($"Scanning and patching type: {targetType.FullName}");
+
+            var plugTypes = _scanner.LoadPlugs(plugAssemblies);
+
+            foreach (var plugType in plugTypes)
+            {
+                // Match if the plug is intended for this target type
+                var plugAttribute = plugType.CustomAttributes
+                    .FirstOrDefault(attr => attr.AttributeType.FullName == typeof(PlugAttribute).FullName);
+
+                if (plugAttribute == null) continue;
+
+                var targetTypeName = plugAttribute.ConstructorArguments.FirstOrDefault().Value?.ToString();
+
+                if (targetTypeName != targetType.FullName) continue;
+
+                foreach (var plugMethod in plugType.Methods)
+                {
+                    if (!plugMethod.IsPublic || !plugMethod.IsStatic) continue;
+
+                    var targetMethod = targetType.Methods.FirstOrDefault(m => m.Name == plugMethod.Name &&
+                                                                             m.Parameters.Count == plugMethod.Parameters.Count);
+                    if (targetMethod != null)
+                    {
+                        Console.WriteLine($"Found matching method: {targetMethod.Name}. Patching...");
+                        PatchMethod(targetMethod, plugMethod);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"No matching method found for plug: {plugMethod.Name}");
+                    }
+                }
+            }
+
+            Console.WriteLine($"Patched type: {targetType.Name} successfully.");
+        }
+
+        /// <summary>
+        /// Patches an entire assembly by scanning for target types and their corresponding plugs.
+        /// </summary>
+        /// <param name="targetAssembly">The assembly to patch.</param>
+        /// <param name="plugAssemblies">The assemblies to search for plug methods.</param>
+        public void PatchAssembly(AssemblyDefinition targetAssembly, params AssemblyDefinition[] plugAssemblies)
+        {
+            if (targetAssembly == null) throw new ArgumentNullException(nameof(targetAssembly));
+            if (plugAssemblies == null || plugAssemblies.Length == 0) throw new ArgumentNullException(nameof(plugAssemblies));
+
+            Console.WriteLine($"Scanning and patching assembly: {targetAssembly.MainModule.Name}");
+
+            foreach (var targetType in targetAssembly.MainModule.Types)
+            {
+                Console.WriteLine($"Processing type: {targetType.FullName}");
+                PatchType(targetType, plugAssemblies);
+            }
+
+            Console.WriteLine($"Patched assembly: {targetAssembly.MainModule.Name} successfully.");
+        }
     }
-    
-    
-    
 }
