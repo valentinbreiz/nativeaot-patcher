08a5740697f28a26a152e6972838d6b793ab25a7
zarlo <git@zarlo.dev>
2025-01-12 15:00:44 +1100

    format project

--- Liquip.NativeWrapper/NativeWrapper.cs
+++ Liquip.NativeWrapper/NativeWrapper.cs
@@ -1,51 +1,63 @@
 ﻿using Liquip.API.Attributes;
 using System.Runtime.InteropServices;
 
-namespace Liquip.NativeWrapper
+namespace Liquip.NativeWrapper;
+
+public class TestClass
 {
-    public class TestClass
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    public static extern int OutputDebugString(string lpOutputString);
+
+    [DllImport("Liquip.NativeLibrary.dll", EntryPoint = "Add", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int Add(int a, int b);
+
+    public static int NativeAdd(int a, int b)
     {
-        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern int OutputDebugString(string lpOutputString);
+        OutputDebugString("NativeAdd method called");
 
-        [DllImport("Liquip.NativeLibrary.dll", EntryPoint = "Add", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int Add(int a, int b);
-
-        public static int NativeAdd(int a, int b)
-        {
-            OutputDebugString("NativeAdd method called");
-
-            return Add(a, b);
-        }
-
-        public static int ManagedAdd(int a, int b)
-        {
-            OutputDebugString("ManagedAdd method called");
-
-            return (a + b);
-        }
+        return Add(a, b);
     }
 
-    public class MockTarget { }
-
-    public class NonPlug { }
-
-    [Plug(typeof(MockTarget))]
-    public class MockPlug { }
-
-    [Plug(typeof(MockTarget))]
-    public class EmptyPlug
+    public static int ManagedAdd(int a, int b)
     {
-        // No methods defined
+        OutputDebugString("ManagedAdd method called");
+
+        return a + b;
+    }
+}
+
+public class MockTarget
+{
+}
+
+public class NonPlug
+{
+}
+
+[Plug(typeof(MockTarget))]
+public class MockPlug
+{
+}
+
+[Plug(typeof(MockTarget))]
+public class EmptyPlug
+{
+    // No methods defined
+}
+
+[Plug(typeof(MockTarget))]
+public class MockPlugWithMethods
+{
+    public static void StaticMethod()
+    {
     }
 
-    [Plug(typeof(MockTarget))]
-    public class MockPlugWithMethods
+    public void InstanceMethod()
     {
-        public static void StaticMethod() { }
-        public void InstanceMethod() { }
     }
+}
 
-    [Plug("OptionalTarget", IsOptional = true)]
-    public class OptionalPlug { }
+[Plug("OptionalTarget", IsOptional = true)]
+public class OptionalPlug
+{
 }
