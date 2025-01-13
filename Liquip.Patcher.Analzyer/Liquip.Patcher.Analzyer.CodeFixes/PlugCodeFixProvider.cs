1058794b889a0d994bc1f94c687a8f563f8258d4
kumja <120261066+kumja1@users.noreply.github.com>
2025-01-13 14:26:25 -0500

    Merge kumja1:feature/analyzer into  valentinbreiz:feature/analyzer (#19)
    * Merge branch 'main' into pr/15
    
    * upd: analzyer files
    
    ---------
    
    Co-authored-by: valentinbreiz <valentinbreiz@gmail.com>

--- Liquip.Patcher.Analzyer/Liquip.Patcher.Analzyer.CodeFixes/PlugCodeFixProvider.cs
+++ Liquip.Patcher.Analzyer/Liquip.Patcher.Analzyer.CodeFixes/PlugCodeFixProvider.cs
@@ -19,21 +19,21 @@ namespace Liquip.Patcher.Analzyer
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
-            get { return ImmutableArray.Create(PlugAnalyzer.DiagnosticId); }
+            get { return ImmutableArray.Create(PatcherAnalyzer.DiagnosticId); }
         }
 
         public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            SyntaxNode? root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
             // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
-            var diagnostic = context.Diagnostics.First();
-            var diagnosticSpan = diagnostic.Location.SourceSpan;
+            Diagnostic diagnostic = context.Diagnostics.First();
+            Microsoft.CodeAnalysis.Text.TextSpan diagnosticSpan = diagnostic.Location.SourceSpan;
 
             // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+            TypeDeclarationSyntax declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
 
             // Register a code action that will invoke the fix.
             context.RegisterCodeFix(
@@ -47,17 +47,17 @@ namespace Liquip.Patcher.Analzyer
         private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
         {
             // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
+            SyntaxToken identifierToken = typeDecl.Identifier;
+            string newName = identifierToken.Text.ToUpperInvariant();
 
             // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+            SemanticModel? semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+            INamedTypeSymbol? typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
 
             // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, new SymbolRenameOptions(), newName, cancellationToken).ConfigureAwait(false);
+            Solution originalSolution = document.Project.Solution;
+            Microsoft.CodeAnalysis.Options.OptionSet optionSet = originalSolution.Workspace.Options;
+            Solution newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, new SymbolRenameOptions(), newName, cancellationToken).ConfigureAwait(false);
 
             // Return the new solution with the now-uppercase type name.
             return newSolution;
