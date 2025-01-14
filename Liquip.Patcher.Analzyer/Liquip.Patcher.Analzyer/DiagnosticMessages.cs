ce69a736a4928bdc972923be76095d283ab2479f
kumja <120261066+kumja1@users.noreply.github.com>
2025-01-14 05:46:36 +0000

    upd: diagnostic messages and codefix

--- Liquip.Patcher.Analzyer/Liquip.Patcher.Analzyer/DiagnosticMessages.cs
+++ Liquip.Patcher.Analzyer/Liquip.Patcher.Analzyer/DiagnosticMessages.cs
@@ -6,7 +6,7 @@ namespace Liquip.Patcher.Analzyer;
 public static class DiagnosticMessages
 {
     public static DiagnosticDescriptor TypeNotFound = new(
-        id: PatcherAnalyzer.DiagnosticId,
+        id: "NAOT0001",
         title: "Type Not Found",
         messageFormat: "The specified type '{0}' could not be located",
         category: "Naming",
@@ -16,24 +16,24 @@ public static class DiagnosticMessages
     );
 
     public static DiagnosticDescriptor MethodNeedsPlug = new(
-        id: PatcherAnalyzer.DiagnosticId,
+        id: "NAOT0002",
         title: "Method Needs Plug",
-        messageFormat: "The method '{0}' requires a plug",
+        messageFormat: "Method '{0}' in plug target {1} requires a plug",
         category: "Usage",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true,
         description: "Ensure that the method has a corresponding plug. See http://www.gocosmos.org/docs/plugs/missing/ for more information."
     );
 
-    public static DiagnosticDescriptor ClassNotStatic = new(
-        id: PatcherAnalyzer.DiagnosticId,
-        title: "Class Not Static",
-        messageFormat: "The plug impl class '{0}' should be static",
+    public static DiagnosticDescriptor PlugNotStatic = new(
+        id: "NAOT0003",
+        title: "Plug Not Static",
+        messageFormat: "Plug '{0}' should be static",
         category: "Design",
-        defaultSeverity: DiagnosticSeverity.Error,
+        defaultSeverity: DiagnosticSeverity.Info,
         isEnabledByDefault: true,
-        description: "Ensure that the class is static if it only contains static members."
+        description: "Ensure that the plug is static if it only contains static members."
     );
 
-    public static ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(TypeNotFound, MethodNeedsPlug, ClassNotStatic);
+    public static ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(TypeNotFound, MethodNeedsPlug, PlugNotStatic);
 }
--- Liquip.Patcher.Analzyer/Liquip.Patcher.Analzyer/PatcherAnalyzer.cs
+++ Liquip.Patcher.Analzyer/Liquip.Patcher.Analzyer/PatcherAnalyzer.cs
@@ -10,7 +10,7 @@ namespace Liquip.Patcher.Analzyer;
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public class PatcherAnalyzer : DiagnosticAnalyzer
 {
-    public const string DiagnosticId = "JJK0001";
+    public const string AnalzyerDiagnosticId = "NAOT";
 
     public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => DiagnosticMessages.SupportedDiagnostics;
 
@@ -20,85 +20,105 @@ public class PatcherAnalyzer : DiagnosticAnalyzer
         context.EnableConcurrentExecution();
 
         context.RegisterSyntaxNodeAction(AnalyzePlugAttribute, SyntaxKind.Attribute);
-        context.RegisterSyntaxNodeAction(AnalyzeMethod, SyntaxKind.MethodDeclaration);
     }
 
-    // Analyze the PlugAttribute
     private void AnalyzePlugAttribute(SyntaxNodeAnalysisContext context)
     {
+        Console.WriteLine("Starting AnalyzePlugAttribute");
+
         if (context.Node is not AttributeSyntax attribute || attribute.Name.ToString() != "Plug")
+        {
+            Console.WriteLine("Attribute is not 'Plug'");
             return;
+        }
 
-        string targetName = GetAttributeValue<string>(attribute, 0);
-        targetName ??= GetAttributeValue<string>(attribute, "TargetName");
+        ClassDeclarationSyntax? plugClass = attribute.FirstAncestorOrSelf<ClassDeclarationSyntax>();
+        AnalyzePlugClass(plugClass, context);
+        Console.WriteLine("Found 'Plug' attribute");
 
+        string targetName = GetAttributeValue<string>(attribute, 0) ?? GetAttributeValue<string>(attribute, "TargetName");
         if (targetName == null)
+        {
+            Console.WriteLine("TargetName is null");
             return;
+        }
 
+        Console.WriteLine($"TargetName: {targetName}");
 
-        // Get the type from the TargetName
         string[] statement = targetName.Trim().Split(',');
         string assemblyName = statement.Last();
         string typeName = statement.First();
 
-        // Check if the type exists
-        var type = Type.GetType(typeName);
-        type ??= Type.GetType(typeName + ", " + assemblyName);
 
-        if (type == null && !GetAttributeValue<bool>(attribute, "IsOptional"))
+        Console.WriteLine($"TypeName: {typeName}, AssemblyName: {assemblyName}");
+
+        bool existInAssembly = context.Compilation.GetTypeByMetadataName(typeName) != null || context.Compilation.ReferencedAssemblyNames.Any(x => x.Name == assemblyName) || context.Compilation.ExternalReferences.Any(x => x.Display.Contains(assemblyName)) || Type.GetType(typeName) != null;
+
+        if (!existInAssembly && !GetAttributeValue<bool>(attribute, "IsOptional"))
         {
+            Console.WriteLine($"Type {typeName} not found and is not optional");
             context.ReportDiagnostic(Diagnostic.Create(DiagnosticMessages.TypeNotFound, attribute.GetLocation(), typeName));
         }
-
-        // Check if the class being plugged is static
-        ClassDeclarationSyntax? classDeclaration = attribute.FirstAncestorOrSelf<ClassDeclarationSyntax>();
-        if (classDeclaration != null && !classDeclaration.Modifiers.Any(SyntaxKind.StaticKeyword))
-        {
-            context.ReportDiagnostic(Diagnostic.Create(DiagnosticMessages.ClassNotStatic, classDeclaration.GetLocation(), classDeclaration.Identifier.Text));
-        }
-        
+        // If the type is found, analyze the plugged class
+        AnalyzePluggedClass(context.Compilation.GetTypeByMetadataName(typeName), plugClass, context);
+        Console.WriteLine("Finished AnalyzePlugAttribute");
     }
 
-    private void AnalyzeMethod(SyntaxNodeAnalysisContext context)
+    private void AnalyzePlugClass(ClassDeclarationSyntax? classDeclarationSyntax, SyntaxNodeAnalysisContext context)
     {
-        MethodDeclarationSyntax method = (context.Node as MethodDeclarationSyntax)!;
-        IMethodSymbol? symbol = context.SemanticModel.GetDeclaredSymbol(method);
+        if (classDeclarationSyntax == null || classDeclarationSyntax.Modifiers.Any(SyntaxKind.StaticKeyword))
+            return;
 
-        if (symbol == null)
+        context.ReportDiagnostic(Diagnostic.Create(DiagnosticMessages.PlugNotStatic, classDeclarationSyntax.GetLocation(), classDeclarationSyntax.Identifier.Text));
+    }
+
+    private void AnalyzePluggedClass(INamedTypeSymbol? symbol, ClassDeclarationSyntax? plugClass, SyntaxNodeAnalysisContext context)
+    {
+        if (plugClass == null || symbol == null)
         {
+            Console.WriteLine("Plug class or symbol is null.");
             return;
         }
 
-        bool needsPlug = false;
-        IEnumerable<AttributeSyntax> attributes = method.AttributeLists.SelectMany(x => x.Attributes);
+        Console.WriteLine($"Analyzing plugged class: {symbol.Name}");
 
-        foreach (AttributeSyntax attribute in attributes)
-        {
-            string name = attribute.Name.ToString();
-            if (name == "DllImport" || name == "LibraryImport")
-            {
-                needsPlug = true;
-                break;
-            }
-            else if (attribute.Name.ToString() == "MethodImpl")
-            {
-                MethodImplOptions value = GetAttributeValue<MethodImplOptions>(attribute, 0);
-                Console.WriteLine(value);
-                bool isInternalCall = value.HasFlag(MethodImplOptions.InternalCall);
-                if (isInternalCall || (isInternalCall && symbol.IsExtern && symbol.IsStatic)) // Check if the method is an InternalCall or Native method
-                {
-                    needsPlug = true;
-                    break;
-                }
-            }
-        }
+        IEnumerable<string> plugMethods = plugClass.Members.OfType<MethodDeclarationSyntax>().Select(x => x.Identifier.Text);
+        Console.WriteLine("Plug methods: " + string.Join(", ", plugMethods));
 
-        // Report diagnostic if it's a DllImport, Native, or InternalCall method
-        if (needsPlug)
+        foreach (IMethodSymbol method in symbol.GetMembers().OfType<IMethodSymbol>().Where(x => x.MethodKind == MethodKind.Ordinary && !plugMethods.Contains(x.Name)))
         {
-            context.ReportDiagnostic(Diagnostic.Create(DiagnosticMessages.MethodNeedsPlug, method.GetLocation(), symbol.Name));
+            Console.WriteLine($"Analyzing method: {method.Name}");
+            ImmutableArray<AttributeData> attributes = method.GetAttributes();
+            bool isInternalCall = attributes.Any(x => x.AttributeClass?.Name == "MethodImpl" && GetAttributeValue<MethodImplOptions>(x, 0).HasFlag(MethodImplOptions.InternalCall));
+            if (isInternalCall || attributes.Any(x => x.AttributeClass?.Name == "DllImport" || x.AttributeClass?.Name == "LibraryImport"))
+            {
+                Console.WriteLine($"Method {method.Name} needs a plug.");
+                context.ReportDiagnostic(Diagnostic.Create(DiagnosticMessages.MethodNeedsPlug, context.Node.GetLocation(), method.Name, symbol.Name));
+            }
         }
     }
+
+    private static T GetAttributeValue<T>(AttributeData attribute, object indexOrString)
+    {
+        if (attribute == null || attribute.ConstructorArguments.Length == 0)
+            return default;
+
+        TypedConstant argument = indexOrString switch
+        {
+            int nameInt when nameInt >= 0 && nameInt < attribute.ConstructorArguments.Length => attribute.ConstructorArguments[nameInt],
+            string nameString => attribute.NamedArguments.FirstOrDefault(x => StringComparer.OrdinalIgnoreCase.Equals(x.Key, nameString)).Value,
+            _ => default,
+        };
+
+        return argument.Value switch
+        {
+            _ when typeof(T).IsEnum => (T)Enum.Parse(typeof(T), argument.Value.ToString()),
+            _ when argument.Value is T value => value,
+            _ when typeof(T) == typeof(Type) => (T)(object)Type.GetType(argument.Value.ToString()),
+            _ => default,
+        };
+    }
+
     private static T GetAttributeValue<T>(AttributeSyntax attribute, object indexOrString)
     {
         if (attribute == null || attribute.ArgumentList == null || attribute.ArgumentList.Arguments.Count == 0)
@@ -106,23 +126,19 @@ public class PatcherAnalyzer : DiagnosticAnalyzer
 
         ExpressionSyntax? argument = indexOrString switch
         {
-            _ when indexOrString is int nameInt && nameInt >= 0 && nameInt < attribute.ArgumentList.Arguments.Count => attribute.ArgumentList.Arguments[nameInt].Expression,
-            _ when indexOrString is string nameString => attribute.ArgumentList.Arguments.FirstOrDefault(x =>
-           {
-               if (x.NameEquals == null)
-                   return false;
-               return StringComparer.OrdinalIgnoreCase.Equals(x.NameEquals.Name.ToString(), nameString);
-           }).Expression,
+            int nameInt when nameInt >= 0 && nameInt < attribute.ArgumentList.Arguments.Count => attribute.ArgumentList.Arguments[nameInt].Expression,
+            string nameString => attribute.ArgumentList.Arguments.FirstOrDefault(x => x.NameEquals != null && StringComparer.OrdinalIgnoreCase.Equals(x.NameEquals.Name.ToString(), nameString))?.Expression,
             _ => default,
         };
-
-        return argument switch
+        var valuea = argument switch
         {
-            _ when typeof(T).IsEnum && argument is MemberAccessExpressionSyntax memberAccess => (T)Enum.Parse(typeof(T), memberAccess.Name.ToString()),
-            _ when argument is LiteralExpressionSyntax literal && literal.Token.Value is T value => value,
+            MemberAccessExpressionSyntax memberAccess when typeof(T).IsEnum => (T)Enum.Parse(typeof(T), memberAccess.Name.ToString()),
+            LiteralExpressionSyntax literal when literal.Token.Value is T value => value,
             _ when typeof(T) == typeof(Type) => (T)(object)Type.GetType(argument.ToString()),
             _ => default,
         };
-    }
+        Console.WriteLine($"Argument: {argument}, Value: {valuea}");
 
+        return valuea;
+    }
 }
