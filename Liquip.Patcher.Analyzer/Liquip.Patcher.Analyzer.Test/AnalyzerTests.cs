e1c9dc3971f91a5ab4cc1eb76370f598d0f92c13
Jesse Nwajiaku <nwajiakumj@gmail.com>
2025-01-21 11:34:40 +0000

    upd: analyzer test

--- Liquip.Patcher.Analyzer/Liquip.Patcher.Analyzer.Test/AnalyzerTests.cs
+++ Liquip.Patcher.Analyzer/Liquip.Patcher.Analyzer.Test/AnalyzerTests.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections.Immutable;
 using Liquip.API.Attributes;
+using Liquip.Patcher.Analyzer;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Xunit;
 
-namespace Liquip.Patcher.Analyzer.Tests;
+namespace Liquip.Patcher.Analzyer.Tests;
 
-    public class AnalyzerTests
+public class AnalyzerTestsTest
+{
+    [Fact]
+    public async Task Test_TypeNotFoundDiagnostic()
     {
-        [Fact]
-        public async Task Test_TypeNotFoundDiagnostic()
-        {
-            string code = @"
+        string code = @"
 using System;
 using Liquip.API.Attributes;
 
@@ -25,14 +26,14 @@ namespace ConsoleApplication1
         public static extern void ExternalMethod();
     }
 }";
-            var diagnostics = await GetDiagnosticsAsync(code);
-            AssertDiagnostic(diagnostics, DiagnosticMessages.TypeNotFound.Id, "System.NonExistent");
-        }
+        ImmutableArray<Diagnostic> diagnostics = await GetDiagnosticsAsync(code);
+        Assert.Contains(diagnostics, d => d.Id == DiagnosticMessages.TypeNotFound.Id && d.GetMessage().Contains("System.NonExistent"));
+    }
 
-        [Fact]
-        public async Task Test_PlugNotStaticDiagnostic()
-        {
-            string code = @"
+    [Fact]
+    public async Task Test_PlugNotStaticDiagnostic()
+    {
+        string code = @"
 using System;
 using Liquip.API.Attributes;
 
@@ -45,20 +46,20 @@ namespace ConsoleApplication1
         public static extern void ExternalMethod();
     }
 }";
-            var diagnostics = await GetDiagnosticsAsync(code);
-            AssertDiagnostic(diagnostics, DiagnosticMessages.PlugNotStatic.Id, "Test");
-        }
+        ImmutableArray<Diagnostic> diagnostics = await GetDiagnosticsAsync(code);
+        Assert.Contains(diagnostics, d => d.Id == DiagnosticMessages.PlugNotStatic.Id && d.GetMessage().Contains("Test"));
+    }
 
-        [Fact]
-        public async Task Test_MethodNeedsPlugDiagnostic()
-        {
-            string code = @"
+    [Fact]
+    public async Task Test_MethodNeedsPlugDiagnostic()
+    {
+        string code = @"
 using System;
 using System.Runtime.CompilerServices;
 using Liquip.API.Attributes;
 
-namespace ConsoleApplication1
-{
+namespace ConsoleApplication1;
+
     public static class TestNativeType
     {
         [DllImport(""example.dll"")]
@@ -71,17 +72,21 @@ namespace ConsoleApplication1
     [Plug(""ConsoleApplication1.TestNativeType"", IsOptional = false)]
     public static class Test
     {
-    }
-}";
-            var diagnostics = await GetDiagnosticsAsync(code);
-            AssertDiagnostic(diagnostics, DiagnosticMessages.MethodNeedsPlug.Id, "ExternalMethod");
-            AssertDiagnostic(diagnostics, DiagnosticMessages.MethodNeedsPlug.Id, "NativeMethod");
-        }
 
-        [Fact]
-        public async Task Test_AnalyzeAccessedMember()
-        {
-            string code = @"
+        
+    }
+";
+        ImmutableArray<Diagnostic> diagnostics = await GetDiagnosticsAsync(code);
+        Assert.Contains(diagnostics, d => d.Id == DiagnosticMessages.MethodNeedsPlug.Id && d.GetMessage().Contains("ExternalMethod"));
+        Assert.Contains(diagnostics, d => d.Id == DiagnosticMessages.MethodNeedsPlug.Id && d.GetMessage().Contains("NativeMethod"));
+
+    }
+
+
+    [Fact]
+    public async Task Test_AnalyzeAccessedMember()
+    {
+        string code = @"
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Liquip.API.Attributes;
@@ -105,51 +110,25 @@ namespace ConsoleApplication1
             TestNativeType.NativeMethod();
         }
     }
-}";
-            var diagnostics = await GetDiagnosticsAsync(code);
-            AssertDiagnostic(diagnostics, DiagnosticMessages.MethodNeedsPlug.Id, "ExternalMethod");
-            AssertDiagnostic(diagnostics, DiagnosticMessages.MethodNeedsPlug.Id, "NativeMethod");
-        }
+}
+";
+        ImmutableArray<Diagnostic> diagnostics = await GetDiagnosticsAsync(code);
 
-        [Fact]
-        public async Task Test_NewDiagnosticScenario()
-        {
-            // Add a new test case to handle scenarios introduced in the update
-            string code = @"
-using Liquip.API.Attributes;
+        Assert.Contains(diagnostics, d => d.Id == DiagnosticMessages.MethodNeedsPlug.Id && d.GetMessage().Contains("ExternalMethod"));
+        Assert.Contains(diagnostics, d => d.Id == DiagnosticMessages.MethodNeedsPlug.Id && d.GetMessage().Contains("NativeMethod"));
+    }
 
-namespace ConsoleApplication1
-{
-    [Plug(""System.NonExistent"", IsOptional = true)]
-    public static class Test
+    private async Task<ImmutableArray<Diagnostic>> GetDiagnosticsAsync(string code)
     {
-        public static void SomeMethod() { }
+        SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
+        CSharpCompilation compilation = CSharpCompilation.Create("TestCompilation")
+            .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
+            .AddReferences(MetadataReference.CreateFromFile(typeof(PlugAttribute).Assembly.Location))
+            .AddSyntaxTrees(syntaxTree);
+
+        PatcherAnalyzer analyzer = new();
+        CompilationWithAnalyzers compilationWithAnalyzers = compilation.WithAnalyzers([analyzer]);
+        return await compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync();
     }
-}";
-            var diagnostics = await GetDiagnosticsAsync(code);
-            AssertDiagnostic(diagnostics, "NewDiagnosticId", "Specific message relevant to new diagnostic");
-        }
-
-        private async Task<ImmutableArray<Diagnostic>> GetDiagnosticsAsync(string code)
-        {
-            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
-
-            var compilation = CSharpCompilation.Create("TestCompilation")
-                .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
-                .AddReferences(MetadataReference.CreateFromFile(typeof(PlugAttribute).Assembly.Location))
-                .AddSyntaxTrees(syntaxTree);
-
-            var analyzer = new PatcherAnalyzer();
-            var compilationWithAnalyzers = compilation.WithAnalyzers(ImmutableArray.Create<DiagnosticAnalyzer>(analyzer));
-
-            return await compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync();
-        }
-
-        private static void AssertDiagnostic(ImmutableArray<Diagnostic> diagnostics, string diagnosticId, string messageFragment)
-        {
-            Assert.Contains(diagnostics, d =>
-                d.Id == diagnosticId &&
-                d.GetMessage().Contains(messageFragment, StringComparison.OrdinalIgnoreCase));
-        }
-    }
+}
 
