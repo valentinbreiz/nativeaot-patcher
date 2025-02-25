0782d431d484e62a6eeeb6a646f3fd653033a264
kumja1 <nwajiakumj@gmail.com>
2025-02-25 03:06:18 +0000

    upd: link and compile files for linux

--- tests/Liquip.NativeWrapper/NativeWrapper.cs
+++ tests/Liquip.NativeWrapper/NativeWrapper.cs
@@ -5,22 +5,22 @@ namespace Liquip.NativeWrapper;
 
 public class TestClass
 {
-    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-    public static extern int OutputDebugString(string lpOutputString);
+   //  [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    // public static extern int OutputDebugString(string lpOutputString);
 
     [DllImport("Liquip.NativeLibrary.dll", EntryPoint = "Add", CallingConvention = CallingConvention.Cdecl)]
     public static extern int Add(int a, int b);
 
     public static int NativeAdd(int a, int b)
     {
-        OutputDebugString("NativeAdd method called");
+       // _ = OutputDebugString("NativeAdd method called");
 
         return Add(a, b);
     }
 
     public static int ManagedAdd(int a, int b)
     {
-        OutputDebugString("ManagedAdd method called");
+        // OutputDebugString("ManagedAdd method called");
 
         return a + b;
     }
