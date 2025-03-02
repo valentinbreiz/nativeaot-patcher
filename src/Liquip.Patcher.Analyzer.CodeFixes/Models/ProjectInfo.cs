2a841675cddc1c6a48577494f417e72202fddf00
kumja1 <nwajiakumj@gmail.com>
2025-03-02 22:15:24 +0000

    upd: remove Polyfill dependency

--- src/Liquip.Patcher.Analyzer.CodeFixes/Models/ProjectInfo.cs
+++ src/Liquip.Patcher.Analyzer.CodeFixes/Models/ProjectInfo.cs
@@ -4,11 +4,12 @@ namespace Liquip.Patcher.Analyzer.CodeFixes.Models;
 
 public readonly record struct ProjectInfo(IEnumerable<string> PlugReferences)
 {
+        public readonly IEnumerable<string> PlugReferences = PlugReferences;
+
         public static ProjectInfo From(XDocument csproj) => new(
             PlugReferences: csproj.Descendants("ItemGroup")
             .Elements("PlugsReference")
             .Select(x => x.Attribute("Include")!.Value)
             .Where(x => x != null)
             );
-
 }
