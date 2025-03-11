ea6cf6528ca192a6c8f93db748a24d417f810fdf
kumja1 <nwajiakumj@gmail.com>
2025-03-11 17:36:13 +0000

    fix: failing test

--- tests/Liquip.ilc.Tests/src/main.cpp
+++ tests/Liquip.ilc.Tests/src/main.cpp
@@ -4,10 +4,12 @@
 // On unix make sure to compile using -ldl and -pthread flags.
 
 // Set this value accordingly to your workspace settings
+#include <cstddef>
+#include <cstdlib>
 #if defined(_WIN32)
-#define PathToLibrary "../Liquip.NativeWrapper/Liquip.NativeWrapper_final.dll"
+#define PathToLibrary "../Liquip.NativeWrapper/bin/Debug/net8.0/native/"
 #else
-#define PathToLibrary "../Liquip.NativeWrapper/bin/Debug/net8.0/native/Liquip.NativeWrapper_final.so"
+#define PathToLibraryDir "../Liquip.NativeWrapper/bin/Debug/net8.0/native/"
 #endif
 
 #ifdef _WIN32
@@ -16,6 +18,7 @@
 #pragma comment(lib, "ole32.lib")
 #else
 #include "dlfcn.h"
+#include "glob.h"
 #include <unistd.h>
 #define symLoad dlsym
 #define CoTaskMemFree free
@@ -29,65 +32,64 @@
 #endif
 
 // Logging function to standard output
-void log_message(const char *message) {
-    printf("[LOG]: %s\n", message);
-}
+void log_message(const char *message) { printf("[LOG]: %s\n", message); }
 
 int callNativeAdd(char *path, char *funcName, int a, int b);
 
 int main() {
-    log_message("Starting the application...");
+  log_message("Starting the application...");
 
-    // Check if the library path exists
-    if (access(PathToLibrary, F_OK) == -1) {
-            printf("PathLibrary: %s\n", PathToLibrary);
+  // Check if the library path exists
+  glob_t glob_result;
+  int len = glob(PathToLibraryDir"*.so", 0, NULL, &glob_result);
+  if (len != 0 && glob_result.gl_pathc <= 0) {
+    log_message("Couldn't find library at the specified path.");
+    return EXIT_FAILURE; 
+  } 
 
-        log_message("Couldn't find library at the specified path.");
-        return 0;
-    }
 
-    printf("PathLibrary: %s\n", PathToLibrary);
+  char* pathToLibrary = glob_result.gl_pathv[0];
+  printf("PathLibrary: %s\n", pathToLibrary);
+  log_message("Attempting to call the native 'Native_Add' function...");
 
-    log_message("Attempting to call the native 'Native_Add' function...");
+  int sum = callNativeAdd(pathToLibrary, "Native_Add", 2, 3);
+  if (sum == -1) {
+    log_message("Failed to call the native function.");
+    return EXIT_FAILURE;
+  }
 
-    int sum = callNativeAdd(PathToLibrary, "Native_Add", 2, 3);
-    if (sum == -1) {
-        log_message("Failed to call the native function.");
-        return -1;
-    }
+  printf("The sum is: %d\n", sum);
+  log_message("Application completed successfully.");
 
-    printf("The sum is: %d\n", sum);
-    log_message("Application completed successfully.");
-
-    return 0;
+  return EXIT_SUCCESS;
 }
 
 int callNativeAdd(char *path, char *funcName, int a, int b) {
-    log_message("Loading the shared library...");
+  log_message("Loading the shared library...");
 
 #ifdef _WIN32
-    HINSTANCE handle = LoadLibraryA(path);
+  HINSTANCE handle = LoadLibraryA(path);
 #else
-    void *handle = dlopen(path, RTLD_LAZY | RTLD_GLOBAL);
+  void *handle = dlopen(path, RTLD_LAZY | RTLD_GLOBAL);
 #endif
 
-    if (!handle) {
-        fprintf(stderr, "dlopen failed: %s\n", dlerror());
-        log_message("Failed to load the shared library.");
-        return -1;
-    }
+  if (!handle) {
+    fprintf(stderr, "dlopen failed: %s\n", dlerror());
+    log_message("Failed to load the shared library.");
+    return -1;
+  }
 
-    log_message("Library loaded successfully.");
+  log_message("Library loaded successfully.");
 
-    typedef int (*myFunc)(int, int);
-    myFunc NativeAdd = (myFunc)symLoad(handle, funcName);
-    if (!NativeAdd) {
-        fprintf(stderr, "dlsym failed: %s\n", dlerror());
-        log_message("Failed to load the function from the library.");
-        return -1;
-    }
+  typedef int (*myFunc)(int, int);
+  myFunc NativeAdd = (myFunc)symLoad(handle, funcName);
+  if (!NativeAdd) {
+    fprintf(stderr, "dlsym failed: %s\n", dlerror());
+    log_message("Failed to load the function from the library.");
+    return -1;
+  }
 
-    log_message("Function loaded successfully. Calling the function...");
+  log_message("Function loaded successfully. Calling the function...");
 
-    return NativeAdd(a, b);
+  return NativeAdd(a, b);
 }
