920f2ca2c90e42686e3de10082f10dc5ee51f280
valentinbreiz <valentinbreiz@gmail.com>
2025-03-29 22:17:43 +0100

    🏗️ Use https://github.com/ilobilo/limine-terminal

--- tests/Cosmos.Kernel.Tests/kernel/src/main.c
+++ tests/Cosmos.Kernel.Tests/kernel/src/main.c
@@ -2,128 +2,156 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <limine.h>
+#include <limine_terminal/term.h>
 
-// Set the base revision to 3, this is recommended as this is the latest
-// base revision described by the Limine boot protocol specification.
-// See specification for further info.
+/* ------------------------
+   Simple bump allocator
+   ------------------------ */
+#define MEMPOOL_SIZE (1024 * 1024)
+static uint8_t mempool[MEMPOOL_SIZE];
+static size_t mempool_offset = 0;
 
-__attribute__((used, section(".limine_requests")))
-static volatile LIMINE_BASE_REVISION(3);
+void *term_alloc(size_t size) {
+    if (mempool_offset + size > MEMPOOL_SIZE)
+        return NULL;
+    void *ptr = &mempool[mempool_offset];
+    mempool_offset += size;
+    return ptr;
+}
 
-// The Limine requests can be placed anywhere, but it is important that
-// the compiler does not optimise them away, so, usually, they should
-// be made volatile or equivalent, _and_ they should be accessed at least
-// once or marked as used with the "used" attribute as done here.
+void *term_realloc(void *oldptr, size_t size) {
+    void *newptr = term_alloc(size);
+    if (!newptr)
+        return NULL;
+    term_memcpy(newptr, oldptr, size);
+    return newptr;
+}
 
-__attribute__((used, section(".limine_requests")))
-static volatile struct limine_framebuffer_request framebuffer_request = {
-    .id = LIMINE_FRAMEBUFFER_REQUEST,
-    .revision = 0
+void term_free(void *ptr, size_t size) {
+    (void)ptr;
+    (void)size;
+}
+
+void term_freensz(void *ptr) {
+    (void)ptr;
+}
+
+void *term_memcpy(void *dest, const void *src, size_t size) {
+    uint8_t *d = (uint8_t *)dest;
+    const uint8_t *s = (const uint8_t *)src;
+    for (size_t i = 0; i < size; i++) {
+        d[i] = s[i];
+    }
+    return dest;
+}
+
+void *term_memset(void *dest, int val, size_t count) {
+    uint8_t *d = (uint8_t *)dest;
+    for (size_t i = 0; i < count; i++) {
+        d[i] = (uint8_t)val;
+    }
+    return dest;
+}
+
+// Implémentation de memset
+void *memset(void *dest, int val, size_t count) {
+    uint8_t *d = (uint8_t *)dest;
+    for (size_t i = 0; i < count; i++) {
+        d[i] = (uint8_t)val;
+    }
+    return dest;
+}
+
+// Implémentation de memcpy
+void *memcpy(void *dest, const void *src, size_t count) {
+    uint8_t *d = (uint8_t *)dest;
+    const uint8_t *s = (const uint8_t *)src;
+    for (size_t i = 0; i < count; i++) {
+        d[i] = s[i];
+    }
+    return dest;
+}
+
+/* ------------------------
+   Configuration de la console et du background
+   ------------------------ */
+struct image_t *image = NULL; // Désactivez le fond pour l'instant
+
+struct framebuffer_t frm = {
+    .address = 0,  // Remplir dynamiquement dans kmain
+    .width = 0,
+    .height = 0,
+    .pitch = 0,
+    .red_mask_size = 0,
+    .red_mask_shift = 0,
+    .green_mask_size = 0,
+    .green_mask_shift = 0,
+    .blue_mask_size = 0,
+    .blue_mask_shift = 0
 };
 
-// Finally, define the start and end markers for the Limine requests.
-// These can also be moved anywhere, to any .c file, as seen fit.
+struct font_t font = {
+    .address = 0, // Pas de police bitmap pour l'instant
+    .width = 8,
+    .height = 16,
+    .spacing = TERM_FONT_SPACING,
+    .scale_x = TERM_FONT_SCALE_X,
+    .scale_y = TERM_FONT_SCALE_Y,
+};
 
-__attribute__((used, section(".limine_requests_start")))
-static volatile LIMINE_REQUESTS_START_MARKER;
+struct style_t style = {
+    .ansi_colours = TERM_ANSI_COLOURS,
+    .ansi_bright_colours = TERM_ANSI_BRIGHT_COLOURS,
+    .background = TERM_BACKGROUND,
+    .foreground = TERM_FOREGROUND_BRIGHT,
+    .background_bright = TERM_BACKGROUND_BRIGHT,
+    .foreground_bright = TERM_FOREGROUND_BRIGHT,
+    .margin = TERM_MARGIN,
+    .margin_gradient = TERM_MARGIN_GRADIENT
+};
 
-__attribute__((used, section(".limine_requests_end")))
-static volatile LIMINE_REQUESTS_END_MARKER;
+struct background_t back = {
+    .background = NULL, // Désactivez le fond
+    .backdrop = TERM_BACKDROP
+};
 
-// GCC and Clang reserve the right to generate calls to the following
-// 4 functions even if they are not directly called.
-// Implement them as the C specification mandates.
-// DO NOT remove or rename these functions, or stuff will eventually break!
-// They CAN be moved to a different .c file.
+/* ------------------------
+   Requête de terminal Limine
+   ------------------------ */
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_terminal_request terminal_request = {
+    .id = LIMINE_TERMINAL_REQUEST,
+    .revision = 1 // Utilisez la révision 1 pour éviter les avertissements
+};
 
-void *memcpy(void *dest, const void *src, size_t n) {
-    uint8_t *pdest = (uint8_t *)dest;
-    const uint8_t *psrc = (const uint8_t *)src;
-
-    for (size_t i = 0; i < n; i++) {
-        pdest[i] = psrc[i];
-    }
-
-    return dest;
-}
-
-void *memset(void *s, int c, size_t n) {
-    uint8_t *p = (uint8_t *)s;
-
-    for (size_t i = 0; i < n; i++) {
-        p[i] = (uint8_t)c;
-    }
-
-    return s;
-}
-
-void *memmove(void *dest, const void *src, size_t n) {
-    uint8_t *pdest = (uint8_t *)dest;
-    const uint8_t *psrc = (const uint8_t *)src;
-
-    if (src > dest) {
-        for (size_t i = 0; i < n; i++) {
-            pdest[i] = psrc[i];
-        }
-    } else if (src < dest) {
-        for (size_t i = n; i > 0; i--) {
-            pdest[i-1] = psrc[i-1];
-        }
-    }
-
-    return dest;
-}
-
-int memcmp(const void *s1, const void *s2, size_t n) {
-    const uint8_t *p1 = (const uint8_t *)s1;
-    const uint8_t *p2 = (const uint8_t *)s2;
-
-    for (size_t i = 0; i < n; i++) {
-        if (p1[i] != p2[i]) {
-            return p1[i] < p2[i] ? -1 : 1;
-        }
-    }
-
-    return 0;
-}
-
-// Halt and catch fire function.
+/* ------------------------
+   Fonction d'arrêt (boucle infinie)
+   ------------------------ */
 static void hcf(void) {
     for (;;) {
-#if defined (__x86_64__)
-        asm ("hlt");
-#elif defined (__aarch64__) || defined (__riscv)
-        asm ("wfi");
-#elif defined (__loongarch64)
-        asm ("idle 0");
+#if defined(__x86_64__)
+        asm("hlt");
+#elif defined(__aarch64__) || defined(__riscv)
+        asm("wfi");
+#elif defined(__loongarch64)
+        asm("idle 0");
 #endif
     }
 }
 
-// The following will be our kernel's entry point.
-// If renaming kmain() to something else, make sure to change the
-// linker script accordingly.
+/* ------------------------
+   Point d'entrée du kernel
+   ------------------------ */
 void kmain(void) {
-    // Ensure the bootloader actually understands our base revision (see spec).
-    if (LIMINE_BASE_REVISION_SUPPORTED == false) {
+    // Initialisation du terminal
+    term_t *term = term_init(frm, font, style, back);
+    if (!term) {
         hcf();
     }
 
-    // Ensure we got a framebuffer.
-    if (framebuffer_request.response == NULL
-     || framebuffer_request.response->framebuffer_count < 1) {
-        hcf();
-    }
+    // Exemple d'écriture sur la console
+    term_write(term, "Hello, World!", 13);
 
-    // Fetch the first framebuffer.
-    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
-
-    // Note: we assume the framebuffer model is RGB with 32-bit pixels.
-    for (size_t i = 0; i < 100; i++) {
-        volatile uint32_t *fb_ptr = framebuffer->address;
-        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
-    }
-
-    // We're done, just hang...
+    // Boucle infinie
     hcf();
 }
