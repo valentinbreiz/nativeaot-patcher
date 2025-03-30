d9d2236545e93bab3a68367ca8e159dba9cd1911
valentinbreiz <valentinbreiz@gmail.com>
2025-03-30 10:09:42 +0200

    🚚 Move files + Add Cosmos.Entry build in Makefile

---
+++ tests/Cosmos.Kernel.Tests/Cosmos.Entry/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Cosmos.Entry
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Cosmos!");
+        }
+    }
+}
