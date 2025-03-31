d8eb05dee9e3cf11ec32f97753e87b7219dc0eb7
valentinbreiz <valentinbreiz@gmail.com>
2025-04-01 00:12:29 +0200

    🏗️ Move to sample C# kernel

--- tests/Cosmos.Kernel.Tests/Cosmos.Native/src/framebuffer.h
+++
@@ -1,9 +0,0 @@
-#ifndef FRAMEBUFFER_H
-#define FRAMEBUFFER_H
-
-#include <limine.h>
-
-// Declare framebuffer_request as an external variable
-extern volatile struct limine_framebuffer_request framebuffer_request;
-
-#endif
