ce8cbf1407dffd75854ea5175dbbc1968def8e8a
valentinbreiz <valentinbreiz@gmail.com>
2025-04-01 00:12:29 +0200

    🏗️ Move to sample C# kernel

---
+++ tests/Cosmos.Kernel.Tests/src/KernelEntry.cs
@@ -0,0 +1,12 @@
+using System.Runtime;
+unsafe class Program
+{
+    [RuntimeExport("kmain")]
+    static void Main() { 
+        // Hello and welcome to kernel-land inside C#!
+
+        // Okay well, we *should* set IDT
+        
+        while (true) ; 
+    }
+}
