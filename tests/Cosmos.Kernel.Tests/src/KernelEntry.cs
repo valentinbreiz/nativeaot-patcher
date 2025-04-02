e77a05475f6274cfbcb31d24096988896c6ec108
valentinbreiz <valentinbreiz@gmail.com>
2025-04-02 16:20:05 +0200

    🐛 Fix limine not finding ELF

--- tests/Cosmos.Kernel.Tests/src/KernelEntry.cs
+++ tests/Cosmos.Kernel.Tests/src/KernelEntry.cs
@@ -3,11 +3,8 @@ using System.Runtime;
 unsafe class Program
 {
     [RuntimeExport("kmain")]
-    static void Main() { 
-        // Hello and welcome to kernel-land inside C#!
-
-        // Okay well, we *should* set IDT
-        
+    static void Main()
+    { 
         while (true) ; 
     }
 }
