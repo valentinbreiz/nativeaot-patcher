1ff4f4bf8f890a6dd7ada1ab88440e869dd4e212
Nathan Hornby <alexmybestcat@gmail.com>
2025-04-08 23:00:09 -0400

    Fixed Hardcoded ASM

--- Cosmos/src/KernelEntry.cs
+++ Cosmos/src/KernelEntry.cs
@@ -50,8 +50,5 @@ public unsafe static class Serial
                 ComWrite((byte)ptr[i]);
             }
         }   
-        Canvas.DrawString("Mreeow :3", 0, 0, Color.White);
-
-        while (true);
     }
 }
