df685b56424c1109272b000e8f9cde82fcbde352
Nathan Hornby <74486606+AzureianGH@users.noreply.github.com>
2025-04-09 18:00:01 -0400

    Update io.asm / Remove Redundant Register Preservation

--- Cosmos/src/Asm/io.asm
+++ Cosmos/src/Asm/io.asm
@@ -12,56 +12,38 @@ section .text
 
 ; void out_byte(ushort port, byte value)
 _native_io_write_byte:
-    push rdx
-    push rax 
     mov     dx, di
     mov     al, sil
     out     dx, al
-    pop     rax
-    pop     rdx
     ret
 
 _native_io_write_word:
-    push rdx
-    push rax 
     mov     dx, di
     mov     ax, si
     out     dx, ax
-    pop rax
-    pop rdx
     ret
 
 ; void out_dword(ushort port, uint value)
 _native_io_write_dword:
-    push rdx
-    push rax 
     mov     dx, di
     mov     eax, esi
     out     dx, eax
-    pop     rax
-    pop     rdx
     ret
 
 ; byte in_byte(ushort port)
 _native_io_read_byte:
-    push rdx
     mov     dx, di
     in      al, dx
-    pop     rdx
     ret
 
 ; ushort in_word(ushort port)
 _native_io_read_word:
-    push rdx
     mov     dx, di
     in      ax, dx
-    pop     rdx
     ret
 
 ; uint in_dword(ushort port)
 _native_io_read_dword:
-    push rdx
     mov     dx, di
     in      eax, dx
-    pop     rdx
     ret
