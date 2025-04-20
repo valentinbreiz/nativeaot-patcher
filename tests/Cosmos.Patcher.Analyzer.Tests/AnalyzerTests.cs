bbdce442c818c8f2e16121e2cc317f467536d9e2
valentinbreiz <valentinbreiz@gmail.com>
2025-04-20 03:50:40 +0200

    Refactor plug system (#82)
    * upd: build targets
    
    * upd: build target
    
    * fix: failing unix test
    
    * upd: windows iso test
    
    * upd: windows iso build
    
    * upd: windows build target
    
    * upd: windows iso build target
    
    * upd: windows iso build target
    
    * upd: windows iso build targets
    
    * upd: windows iso build target
    
    * upd: windows iso build targets
    
    * upd: windows iso build target
    
    * upd: windows iso build targets
    
    * upd: push changes(not finished)
    
    * upd: restructure project structure
    
    * upd: add PlugMemberAttribute
    
    * upd: patch class properties and fields
    
    * upd: fix field patching
    
    * upd: fix property patching
    
    * upd: fix ReplaceFieldAccess logic
    
    * upd: add PlugPatcher.cs back
    
    * fix: project paths in test
    
    * upd: Common.Build.targets
    
    * upd: dotnet.yml
    
    * fix: unix-iso-tests
    
    * fix: unix-iso-tests
    
    * upd: dotnet.yml
    
    * upd: build targets
    
    * upd: normalize obj file path
    
    * upd: remove Cosmos.Asm.Build.Windows
    
    * upd: dotnet.yml
    
    * upd: add asm platform targets
    
    * upd: build targets
    
    * upd: build targets
    
    * upd: build targets
    
    * upd: build targets
    
    * upd: build targets
    
    * upd: build targets
    
    * upd: remove windows-iso test for now
    
    * upd: format files
    
    ---------
    
    Co-authored-by: kumja1 <nwajiakumj@gmail.com>
    Co-authored-by: kumja <120261066+kumja1@users.noreply.github.com>

---
+++ tests/Cosmos.Patcher.Analyzer.Tests/AnalyzerTests.cs
@@ -0,0 +1,220 @@
+using System.Collections.Immutable;
+using Cosmos.API.Attributes;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace Cosmos.Patcher.Analyzer.Tests;
+
+public class AnalyzerTests
+{
+    private static readonly MetadataReference CorlibReference =
+        MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+
+    private static readonly MetadataReference PlugAttributeReference =
+        MetadataReference.CreateFromFile(typeof(PlugAttribute).Assembly.Location);
+
+    private static SyntaxTree ParseCode(string code) => CSharpSyntaxTree.ParseText(code);
+
+
+    [Fact]
+    public async Task Test_TypeNotFoundDiagnostic()
+    {
+        const string code = """
+                            using System;
+                            using Cosmos.API.Attributes;
+
+                            namespace ConsoleApplication1
+                            {
+                                [Plug("System.NonExistent", IsOptional = false)]
+                                public static class Test
+                                {
+                                    [DllImport("example.dll")]
+                                    public static extern void ExternalMethod();
+                                }
+                            }
+                            """;
+        ImmutableArray<Diagnostic> diagnostics = await GetDiagnosticsAsync(code);
+        Assert.Contains(diagnostics,
+            d => d.Id == DiagnosticMessages.TypeNotFound.Id && d.GetMessage().Contains("System.NonExistent"));
+    }
+
+    [Fact]
+    public async Task Test_PlugNotStaticDiagnostic()
+    {
+        const string code = """
+                            using System;
+                            using Cosmos.API.Attributes;
+
+                            namespace ConsoleApplication1
+                            {
+                                [Plug("System.String", IsOptional = true)]
+                                public class Test
+                                {
+                                    [DllImport("example.dll")]
+                                    public static extern void ExternalMethod();
+                                }
+                            }
+                            """;
+        ImmutableArray<Diagnostic> diagnostics = await GetDiagnosticsAsync(code);
+        Assert.Contains(diagnostics,
+            d => d.Id == DiagnosticMessages.PlugNotStatic.Id && d.GetMessage().Contains("Test"));
+    }
+
+
+    private static async Task<ImmutableArray<Diagnostic>> GetDiagnosticsAsync(string code)
+    {
+        SyntaxTree syntaxTree = ParseCode(code);
+
+        CSharpCompilation compilation = CSharpCompilation.Create("TestCompilation")
+            .AddReferences(CorlibReference, PlugAttributeReference)
+            .AddSyntaxTrees(syntaxTree);
+
+        PatcherAnalyzer analyzer = new();
+        CompilationWithAnalyzers compilationWithAnalyzers =
+            compilation.WithAnalyzers(ImmutableArray.Create<DiagnosticAnalyzer>(analyzer));
+        return await compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync();
+    }
+
+
+    [Fact]
+    public async Task Test_AnalyzeAccessedMember()
+    {
+        const string code = @"
+        using System.Runtime.CompilerServices;
+        using System.Runtime.InteropServices;
+        using Cosmos.API.Attributes;
+
+        namespace ConsoleApplication1
+        {
+            public static class TestNativeType
+            {
+                [DllImport(""example.dll"")]
+                public static extern void ExternalMethod();
+
+                [MethodImpl(MethodImplOptions.InternalCall)]
+                public static extern void NativeMethod();
+            }
+
+            public class Test
+            {
+                public void TestMethod()
+                {
+                    TestNativeType.ExternalMethod();
+                    TestNativeType.NativeMethod();
+                }
+            }
+        }";
+
+        ImmutableArray<Diagnostic> diagnostics = await GetDiagnosticsAsync(code);
+
+        Assert.Contains(diagnostics,
+            d => d.Id == DiagnosticMessages.MethodNeedsPlug.Id && d.GetMessage().Contains("ExternalMethod"));
+        Assert.Contains(diagnostics,
+            d => d.Id == DiagnosticMessages.MethodNeedsPlug.Id && d.GetMessage().Contains("NativeMethod"));
+    }
+
+    [Fact]
+    public async Task Test_MethodNotImplemented()
+    {
+        const string code = """
+        using System.Runtime.CompilerServices;
+        using System.Runtime.InteropServices;
+        using Cosmos.API.Attributes;
+
+        namespace ConsoleApplication1
+        {
+            public static class TestNativeType
+            {
+                [DllImport("example.dll")]
+                public static extern void ExternalMethod();
+
+                [MethodImpl(MethodImplOptions.InternalCall)]
+                public static extern void NativeMethod();
+            }
+
+            [Plug("ConsoleApplication1.TestNativeType", IsOptional = false)]
+            public static class Test
+            {
+                public static void NotImplemented() {}
+            }
+        }
+""";
+
+        ImmutableArray<Diagnostic> diagnostics = await GetDiagnosticsAsync(code);
+
+        Assert.Contains(diagnostics,
+            d => d.Id == DiagnosticMessages.MethodNotImplemented.Id && d.GetMessage().Contains("NotImplemented"));
+    }
+
+    [Fact]
+    public async Task Test_StaticConstructorTooManyParameters()
+    {
+        const string code = """
+
+        using System.Runtime.CompilerServices;
+        using System.Runtime.InteropServices;
+        using Cosmos.API.Attributes;
+
+        namespace ConsoleApplication1
+        {
+            public static class TestNativeType
+            {
+                  [DllImport("example.dll")]
+                public static extern void ExternalMethod();
+
+                [MethodImpl(MethodImplOptions.InternalCall)]
+                public static extern void NativeMethod();
+                
+                static TestNativeType() => Console.WriteLine("123");
+            }
+
+            [Plug("ConsoleApplication1.TestNativeType", IsOptional = false)]
+            public static class Test
+            {
+                public static void CCtor(object aThis, object param) => Console.WriteLine(param);
+            }
+        }
+""";
+
+        ImmutableArray<Diagnostic> diagnostics = await GetDiagnosticsAsync(code);
+
+        Assert.Contains(diagnostics,
+            d => d.Id == DiagnosticMessages.StaticConstructorTooManyParams.Id && d.GetMessage().Contains("CCtor"));
+    }
+
+    [Fact]
+    public async Task Test_StaticConstructorNotImplemented()
+    {
+        const string code = """
+
+        using System.Runtime.CompilerServices;
+        using System.Runtime.InteropServices;
+        using Cosmos.API.Attributes;
+
+        namespace ConsoleApplication1
+        {
+            public static class TestNativeType
+            {
+                  [DllImport("example.dll")]
+                public static extern void ExternalMethod();
+
+                [MethodImpl(MethodImplOptions.InternalCall)]
+                public static extern void NativeMethod();
+                            }
+
+            [Plug("ConsoleApplication1.TestNativeType", IsOptional = false)]
+            public static class Test
+            {
+                public static void CCtor(object param, object the) => Console.WriteLine(param);
+            }
+        }
+""";
+
+        ImmutableArray<Diagnostic> diagnostics = await GetDiagnosticsAsync(code);
+
+        Assert.Contains(diagnostics,
+            d => d.Id == DiagnosticMessages.MethodNotImplemented.Id && d.GetMessage().Contains(".cctor"));
+    }
+}
