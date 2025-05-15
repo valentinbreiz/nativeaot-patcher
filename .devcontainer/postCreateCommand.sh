e7b99fbe246a593346ac9d70f0cb81db0ff14629
Guillermo Santos <96314559+Guillermo-Santos@users.noreply.github.com>
2025-05-15 17:51:59 +0000

    Add postcreateCommand.sh to devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+ls -la
+
+# Pack all projects
+dotnet build ./src/Cosmos.API/Cosmos.API.csproj --configuration Release
+dotnet build ./src/Cosmos.Patcher.Build/Cosmos.Patcher.Build.csproj --configuration Release
+dotnet build ./src/Cosmos.Patcher/Cosmos.Patcher.csproj --configuration Release
+dotnet build ./src/Cosmos.Common.Build/Cosmos.Common.Build.csproj --configuration Release
+dotnet build ./src/Cosmos.Ilc.Build/Cosmos.Ilc.Build.csproj --configuration Release
+dotnet build ./src/Cosmos.Asm.Build/Cosmos.Asm.Build.csproj --configuration Release
+dotnet build ./src/Cosmos.Patcher.Analyzer.Package/Cosmos.Patcher.Analyzer.Package.csproj --configuration Release
+
+# Add output folder as a local NuGet source
+dotnet nuget add source "$PWD/artifacts/package/release" --name local-packages
+
+# Clear all NuGet locals cache
+dotnet nuget locals all --clear
+
+dotnet restore
