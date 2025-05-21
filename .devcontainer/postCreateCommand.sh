a5f025e66b49006f58fc17d57cb7985c1b4fa302
Guillermo Santos <96314559+Guillermo-Santos@users.noreply.github.com>
2025-05-21 04:13:12 +0000

    make postCreateCommand reusable on devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -12,8 +12,12 @@ dotnet build ./src/Cosmos.Asm.Build/Cosmos.Asm.Build.csproj --configuration Rele
 dotnet build ./src/Cosmos.Patcher.Analyzer.Package/Cosmos.Patcher.Analyzer.Package.csproj --configuration Release
 dotnet build ./src/Cosmos.Sdk/Cosmos.Sdk.csproj --configuration Release
 
-# Add output folder as a local NuGet source
-dotnet nuget add source "$PWD/artifacts/package/release" --name local-packages
+# Add output folder as a local NuGet source if it doesn't already exist
+if ! dotnet nuget list source | grep -q "local-packages"; then
+  dotnet nuget add source "$PWD/artifacts/package/release" --name local-packages
+else
+  echo "[DEBUG] NuGet source 'local-packages' already exists."
+fi
 
 # Clear all NuGet locals cache
 dotnet nuget locals all --clear
