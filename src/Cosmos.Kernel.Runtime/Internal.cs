ce40a3fabf00e8fc41d91f090ff71eabd168e45e
kumja1 <nwajiakumj@gmail.com>
2025-07-02 02:34:41 -0400

    fix: errors in files

--- src/Cosmos.Kernel.Runtime/Internal.cs
+++ src/Cosmos.Kernel.Runtime/Internal.cs
@@ -1,39 +1,7 @@
+using System;
+using System.Runtime;
+using System.Runtime.CompilerServices;
 namespace Cosmos.Kernel.Runtime;
-
-internal sealed class RuntimeExportAttribute(string entry) : Attribute
-{
-}
-
-internal sealed class RuntimeImportAttribute : Attribute
-{
-    public string DllName { get; }
-    public string EntryPoint { get; }
-
-    public RuntimeImportAttribute(string entry)
-    {
-        EntryPoint = entry;
-    }
-
-    public RuntimeImportAttribute(string dllName, string entry)
-    {
-        EntryPoint = entry;
-        DllName = dllName;
-    }
-}
-
-// A class that the compiler looks for that has helpers to initialize the
-// process. The compiler can gracefully handle the helpers not being present,
-// but the class itself being absent is unhandled. Let's add an empty class.
-class StartupCodeHelpers
-{
-    // A couple symbols the generated code will need we park them in this class
-    // for no particular reason. These aid in transitioning to/from managed code.
-    // Since we don't have a GC, the transition is a no-op.
-    [RuntimeExport("RhpReversePInvoke")]
-    static void RhpReversePInvoke(IntPtr frame) { }
-    [RuntimeExport("RhpReversePInvokeReturn")]
-    static void RhpReversePInvokeReturn(IntPtr frame) { }
-}
 
 public static class Native
 {
@@ -62,46 +30,5 @@ public static class Native
         [MethodImpl(MethodImplOptions.InternalCall)]
         [RuntimeImport("*", "_native_io_read_dword")]
         public static extern uint Read32(ushort Port);
-    }
-}
-
-public static class Serial
-{
-    private static readonly ushort COM1 = 0x3F8;
-
-    private static void WaitForTransmitBufferEmpty()
-    {
-        while ((Native.IO.Read8((ushort)(COM1 + 5)) & 0x20) == 0) ;
-    }
-
-    public static void ComWrite(byte value)
-    {
-        // Wait for the transmit buffer to be empty
-        WaitForTransmitBufferEmpty();
-        // Write the byte to the COM port
-        Native.IO.Write8(COM1, value);
-    }
-
-
-
-    public static void ComInit()
-    {
-        Native.IO.Write8((ushort)(COM1 + 1), 0x00);
-        Native.IO.Write8((ushort)(COM1 + 3), 0x80);
-        Native.IO.Write8(COM1, 0x01);
-        Native.IO.Write8((ushort)(COM1 + 1), 0x00);
-        Native.IO.Write8((ushort)(COM1 + 3), 0x03);
-        Native.IO.Write8((ushort)(COM1 + 2), 0xC7);
-    }
-
-    public static unsafe void WriteString(string str)
-    {
-        fixed (char* ptr = str)
-        {
-            for (int i = 0; i < str.Length; i++)
-            {
-                ComWrite((byte)ptr[i]);
-            }
-        }
     }
 }
--- src/Cosmos.Kernel.System/IO/Serial.cs
+++ src/Cosmos.Kernel.System/IO/Serial.cs
@@ -1,42 +1,42 @@
 using System;
+using Cosmos.Kernel.Runtime;
 
-namespace Cosmos.Kernel.System.IO
+namespace Cosmos.Kernel.System.IO;
+
+public static class Serial
 {
-    public static class Serial
+    private static readonly ushort COM1 = 0x3F8;
+
+    private static void WaitForTransmitBufferEmpty()
     {
-        private static readonly ushort COM1 = 0x3F8;
+        while ((Native.IO.Read8((ushort)(COM1 + 5)) & 0x20) == 0) ;
+    }
 
-        private static void WaitForTransmitBufferEmpty()
-        {
-            while ((Native.IO.Read8((ushort)(COM1 + 5)) & 0x20) == 0) ;
-        }
+    public static void ComWrite(byte value)
+    {
+        // Wait for the transmit buffer to be empty
+        WaitForTransmitBufferEmpty();
+        // Write the byte to the COM port
+        Native.IO.Write8(COM1, value);
+    }
 
-        public static void ComWrite(byte value)
-        {
-            // Wait for the transmit buffer to be empty
-            WaitForTransmitBufferEmpty();
-            // Write the byte to the COM port
-            Native.IO.Write8(COM1, value);
-        }
+    public static void ComInit()
+    {
+        Native.IO.Write8((ushort)(COM1 + 1), 0x00);
+        Native.IO.Write8((ushort)(COM1 + 3), 0x80);
+        Native.IO.Write8(COM1, 0x01);
+        Native.IO.Write8((ushort)(COM1 + 1), 0x00);
+        Native.IO.Write8((ushort)(COM1 + 3), 0x03);
+        Native.IO.Write8((ushort)(COM1 + 2), 0xC7);
+    }
 
-        public static void ComInit()
+    public static unsafe void WriteString(string str)
+    {
+        fixed (char* ptr = str)
         {
-            Native.IO.Write8((ushort)(COM1 + 1), 0x00);
-            Native.IO.Write8((ushort)(COM1 + 3), 0x80);
-            Native.IO.Write8(COM1, 0x01);
-            Native.IO.Write8((ushort)(COM1 + 1), 0x00);
-            Native.IO.Write8((ushort)(COM1 + 3), 0x03);
-            Native.IO.Write8((ushort)(COM1 + 2), 0xC7);
-        }
-
-        public static unsafe void WriteString(string str)
-        {
-            fixed (char* ptr = str)
+            for (int i = 0; i < str.Length; i++)
             {
-                for (int i = 0; i < str.Length; i++)
-                {
-                    ComWrite((byte)ptr[i]);
-                }
+                ComWrite((byte)ptr[i]);
             }
         }
     }
