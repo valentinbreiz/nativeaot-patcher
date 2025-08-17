5b61ae0be703d96cd44c157d1c0ce11815b0a690
Guillermo Santos <santosguillermo360@outlook.com>
2025-08-17 00:19:05 +0000

    🔥 ISO Compilation with default CoreLib

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,11 +6,12 @@ set -e
 dotnet build ./src/Cosmos.Build.API/Cosmos.Build.API.csproj --configuration Release
 dotnet build ./src/Cosmos.Build.Patcher/Cosmos.Build.Patcher.csproj --configuration Release
 dotnet build ./src/Cosmos.Patcher/Cosmos.Patcher.csproj --configuration Release
+dotnet build ./src/Cosmos.Patcher/Cosmos.Patcher.csproj --configuration Release
 dotnet build ./src/Cosmos.Build.Common/Cosmos.Build.Common.csproj --configuration Release
 dotnet build ./src/Cosmos.Build.Ilc/Cosmos.Build.Ilc.csproj --configuration Release
 dotnet build ./src/Cosmos.Build.Asm/Cosmos.Build.Asm.csproj --configuration Release
 dotnet build ./src/Cosmos.Build.Analyzer.Patcher.Package/Cosmos.Build.Analyzer.Patcher.Package.csproj --configuration Release
-dotnet build ./src/Cosmos.Sdk/Cosmos.Sdk.csproj --configuration Release
+dotnet build ./src/Cosmos.Kernel.Native.x86/Cosmos.Kernel.Native.x86.csproj --configuration Release
 
 # Add output folder as a local NuGet source if it doesn't already exist
 if ! dotnet nuget list source | grep -q "local-packages"; then
