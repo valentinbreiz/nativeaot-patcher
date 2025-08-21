578731de91c695bf38cfbb54344a38c232bd3afc
valentinbreiz <valentinbreiz@gmail.com>
2025-08-22 01:46:00 +0200

    ✨ Add PlugReference

--- src/Cosmos.Kernel.Plugs/System/ConsolePlug.cs
+++ src/Cosmos.Kernel.Plugs/System/ConsolePlug.cs
@@ -7,9 +7,18 @@ namespace Cosmos.Kernel.Plugs.System;
 [Plug(typeof(Console))]
 public static class ConsolePlug
 {
+    [PlugMember]
     public static void Write(string value) => KernelConsole.Write(value);
+
+    [PlugMember]
     public static void WriteLine(string value) => KernelConsole.WriteLine(value);
+
+    [PlugMember]
     public static void Write(char value) => KernelConsole.Write(value);
+
+    [PlugMember]
     public static void WriteLine(char value) => KernelConsole.WriteLine(value);
+
+    [PlugMember]
     public static void WriteLine() => KernelConsole.WriteLine();
 }
