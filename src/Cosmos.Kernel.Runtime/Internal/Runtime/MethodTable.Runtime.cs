578cda3bdfa7ff7c9ed9e1d7d967c0567b4830b5
Guillermo Santos <santosguillermo360@outlook.com>
2025-09-17 04:00:48 +0000

    Add dotnet method table
    We also implement TypeManager and TypeManagerHandle

---
+++ src/Cosmos.Kernel.Runtime/Internal/Runtime/MethodTable.Runtime.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using Cosmos.Kernel.Runtime;
+
+namespace Internal.Runtime;
+
+
+internal unsafe partial struct MethodTable
+{
+    internal MethodTable* GetArrayEEType()
+    {
+        void* pGetArrayEEType = ModuleHelpers.RhpGetClasslibFunctionFromEEType((MethodTable*)Unsafe.AsPointer(ref this), ClassLibFunctionId.GetSystemArrayEEType);
+        return ((delegate* <MethodTable*>)pGetArrayEEType)();
+        return MethodTable.Of<Array>();
+    }
+    /*
+    internal Exception GetClasslibException(ExceptionIDs id)
+    {
+        if (IsParameterizedType)
+        {
+            return RelatedParameterType->GetClasslibException(id);
+        }
+
+        return EH.GetClasslibExceptionFromEEType(id, (MethodTable*)Unsafe.AsPointer(ref this));
+    }
+    */
+    internal IntPtr GetClasslibFunction(ClassLibFunctionId id)
+    {
+        return (IntPtr)ModuleHelpers.RhpGetClasslibFunctionFromEEType((MethodTable*)Unsafe.AsPointer(ref this), id);
+    }
+}
