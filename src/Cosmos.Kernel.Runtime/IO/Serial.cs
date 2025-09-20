95f579e117e311f685ee3b4a48b43cb1968757cc
valentinbreiz <valentinbreiz@gmail.com>
2025-09-20 19:34:30 +0200

    ✨ Add no allocation serial debug

--- src/Cosmos.Kernel.Runtime/IO/Serial.cs
+++ src/Cosmos.Kernel.Runtime/IO/Serial.cs
@@ -39,4 +39,80 @@ public static class Serial
             }
         }
     }
+
+    public static unsafe void WriteNumber(ulong number, bool hex = false)
+    {
+        if (number == 0)
+        {
+            ComWrite((byte)'0');
+            return;
+        }
+
+        const int maxDigits = 20; // Enough for 64-bit numbers
+        byte* buffer = stackalloc byte[maxDigits];
+        int index = 0;
+        ulong baseValue = hex ? 16u : 10u;
+
+        while (number > 0)
+        {
+            ulong digit = number % baseValue;
+            if (hex && digit >= 10)
+            {
+                buffer[index] = (byte)('A' + (digit - 10));
+            }
+            else
+            {
+                buffer[index] = (byte)('0' + digit);
+            }
+            number /= baseValue;
+            index++;
+        }
+
+        // Write digits in reverse order
+        for (int i = index - 1; i >= 0; i--)
+        {
+            ComWrite(buffer[i]);
+        }
+    }
+
+    public static void WriteNumber(uint number, bool hex = false)
+    {
+        WriteNumber((ulong)number, hex);
+    }
+
+    public static void WriteNumber(int number, bool hex = false)
+    {
+        if (number < 0)
+        {
+            ComWrite((byte)'-');
+            WriteNumber((ulong)(-number), hex);
+        }
+        else
+        {
+            WriteNumber((ulong)number, hex);
+        }
+    }
+
+    public static void WriteNumber(long number, bool hex = false)
+    {
+        if (number < 0)
+        {
+            ComWrite((byte)'-');
+            WriteNumber((ulong)(-number), hex);
+        }
+        else
+        {
+            WriteNumber((ulong)number, hex);
+        }
+    }
+
+    public static void WriteHex(ulong number)
+    {
+        WriteNumber(number, true);
+    }
+
+    public static void WriteHex(uint number)
+    {
+        WriteNumber((ulong)number, true);
+    }
 }
