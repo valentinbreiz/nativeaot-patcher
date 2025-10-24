47329caa8caec913f1f4d07da7aebebddf4779c8
Guillermo Santos <96314559+Guillermo-Santos@users.noreply.github.com>
2025-10-24 07:58:10 -0400

    Feature: Read Embedded Resources (#176)
    * Implement Resource Manager
    
    This class allow us to access embedded resource files, either as a Stream or as a Span.
    
    * Plug AppContext
    
    * Add missing dependencies
    
    * Move Default psf font to embedded resource

---
+++ src/Cosmos.Kernel.Core/Utilities/SimpleDictionary.cs
@@ -0,0 +1,181 @@
+using Cosmos.Kernel.System.IO;
+
+namespace Cosmos.Kernel.Core.Utilities;
+
+public class SimpleDictionary<TKey, TValue> where TKey : notnull
+{
+    private const int InitialCapacity = 16;
+    private Entry[] _buckets;
+    private int _count;
+
+    public SimpleDictionary() : this(InitialCapacity) { }
+    public SimpleDictionary(int capacity)
+    {
+        _buckets = new Entry[capacity];
+    }
+
+
+    public int Count => _count;
+
+    public TValue this[TKey key]
+    {
+        get
+        {
+            if (TryGetValue(key, out TValue value))
+            {
+                return value;
+            }
+
+            throw new KeyNotFoundException();
+        }
+        set
+        {
+            Add(key, value);
+        }
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        int hash = HashCode(key);
+        int bucketIndex = hash % _buckets.Length;
+
+        var newEntry = new Entry(key, value);
+
+        if (_buckets[bucketIndex] == null)
+        {
+            _buckets[bucketIndex] = newEntry;
+        }
+        else
+        {
+            Entry? current = _buckets[bucketIndex];
+            while (current is not null)
+            {
+                if (current.Key.Equals(key))
+                {
+                    throw new ArgumentException("An item with the same key already exists.");
+                }
+
+                if (current.Next == null)
+                {
+                    current.Next = newEntry;
+                    break;
+                }
+
+                current = current.Next;
+            }
+        }
+
+        _count++;
+
+        // Resize if needed
+        if (_count > _buckets.Length * 0.75)
+        {
+            Resize();
+        }
+    }
+
+    public bool TryGetValue(TKey key, out TValue value)
+    {
+        int hash = HashCode(key);
+        int bucketIndex = hash % _buckets.Length;
+
+        var current = _buckets[bucketIndex];
+
+        while (current != null)
+        {
+            if (current.Key.Equals(key))
+            {
+                value = current.Value;
+                return true;
+            }
+
+            current = current.Next;
+        }
+
+        value = default!;
+        return false;
+    }
+
+    public bool ContainsKey(TKey key)
+    {
+        return TryGetValue(key, out _);
+    }
+
+    private void Resize()
+    {
+        var oldBuckets = _buckets;
+        _buckets = new Entry[_buckets.Length * 2];
+        _count = 0;
+
+        foreach (var entry in oldBuckets)
+        {
+            var current = entry;
+            while (current != null)
+            {
+                Add(current.Key, current.Value);
+                current = current.Next;
+            }
+        }
+    }
+
+    private static int HashCode(TKey key)
+    {
+        if (key is string strKey)
+        {
+            unchecked
+            {
+                int hash = 17;
+                foreach (char c in strKey)
+                {
+                    hash = hash * 31 + c;
+                }
+                hash &= 0x7FFFFFFF;
+                return hash;
+            }
+        }
+        else
+        {
+            return key.GetHashCode() & 0x7FFFFFFF;
+        }
+    }
+
+    public bool Remove(TKey key)
+    {
+        int hash = HashCode(key);
+        int bucketIndex = hash % _buckets.Length;
+
+        Entry? current = _buckets[bucketIndex];
+        Entry? previous = null;
+
+        while (current != null)
+        {
+            if (current.Key.Equals(key))
+            {
+                if (previous == null)
+                {
+                    _buckets[bucketIndex] = current.Next!;
+                }
+                else
+                {
+                    previous.Next = current.Next;
+                }
+
+                _count--;
+                return true;
+            }
+
+            previous = current;
+            current = current.Next;
+        }
+
+        return false;
+    }
+    public void Add(KeyValuePair<TKey, TValue> item) => Add(item.Key, item.Value);
+
+    private sealed class Entry(TKey key, TValue value)
+    {
+        public TKey Key { get; } = key;
+        public TValue Value { get; } = value;
+        public Entry? Next { get; set; }
+    }
+}
---
+++ src/Cosmos.Kernel.Plugs/System/AppContextPlug.cs
@@ -0,0 +1,125 @@
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Text.Unicode;
+using Cosmos.Build.API.Attributes;
+using Cosmos.Kernel.Core.Utilities;
+using Cosmos.Kernel.System.Graphics;
+using Cosmos.Kernel.System.IO;
+
+namespace Cosmos.Kernel.Plugs.System;
+
+[Plug(typeof(AppContext))]
+public static partial class AppContextPlug
+{
+    [LibraryImport("*", EntryPoint = "RhGetKnobValues")]
+    private static unsafe partial uint RhGetKnobValues(out byte** keys, out byte** values);
+    private static SimpleDictionary<string, object?>? dataStore;
+    private static SimpleDictionary<string, bool>? switches;
+
+    [PlugMember]
+    public static void EnsureInitialized()
+    {
+        if (dataStore is not null) return;
+        unsafe
+        {
+            uint count = RhGetKnobValues(out byte** knobKeys, out byte** knobValues);
+
+            dataStore = new(capacity: (int)count);
+            switches = new();
+
+            for (int i = 0; i < count; i++)
+            {
+                byte* ptrKey = knobKeys[i];
+                byte* ptrVal = knobValues[i];
+
+                string key = Utf8Decode(new(ptrKey, Strlen(ptrKey)));
+                string value = Utf8Decode(new(ptrVal, Strlen(ptrVal)));
+
+                Serial.WriteString(key);
+                Serial.WriteString(" = ");
+                Serial.WriteString(value + "\n");
+
+                dataStore[key] = value;
+
+                if (bool.TryParse(value, out bool result))
+                {
+                    switches.Add(key, result);
+                }
+            }
+        }
+    }
+
+
+
+    [PlugMember]
+    public static bool TryGetSwitch(string switchName, out bool isEnabled)
+    {
+        EnsureInitialized();
+
+        ArgumentException.ThrowIfNullOrEmpty(switchName);
+
+        if (switches != null)
+        {
+            if (switches.TryGetValue(switchName, out isEnabled))
+                return true;
+        }
+
+        var data = GetData(switchName);
+
+        if (GetData(switchName) is string value && bool.TryParse(value, out isEnabled))
+        {
+            return true;
+        }
+
+        isEnabled = false;
+        return false;
+    }
+
+    [PlugMember]
+    public static object? GetData(string name)
+    {
+        EnsureInitialized();
+        Serial.WriteString("Getting Data!\n");
+
+        dataStore!.TryGetValue(name, out object? data);
+        Serial.WriteString((string)data);
+        return data;
+    }
+    [PlugMember]
+    public static void SetData(string switchName, object? data)
+    {
+        EnsureInitialized();
+
+        dataStore![switchName] = data;
+    }
+
+    [PlugMember]
+    public static void SetSwitch(string switchName, bool isEnabled)
+    {
+        EnsureInitialized();
+
+        dataStore![switchName] = isEnabled;
+    }
+
+
+    internal unsafe static int Strlen(byte* str, int max = int.MaxValue)
+    {
+        int length = 0;
+        while (str[length] != 0x00 && length < max)
+        {
+            length++;
+        }
+        return length;
+    }
+
+    internal static string Utf8Decode(ReadOnlySpan<byte> bytes)
+    {
+        Span<char> buffer = stackalloc char[bytes.Length];
+        return Utf8.ToUtf16(bytes, buffer, out _, out int charsWritten) switch
+        {
+            global::System.Buffers.OperationStatus.Done => new string(buffer.Slice(0, charsWritten)),
+            _ => string.Empty,
+        };
+    }
+}
