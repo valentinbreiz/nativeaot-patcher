0b94e6bccec54f9633fa541ee9442f803ab2b12c
valentinbreiz <valentinbreiz@gmail.com>
2025-10-24 04:22:44 +0200

    🔥 Remove non working code (blockdevice)

--- src/Cosmos.Kernel.HAL/BlockDevice/BlockDeviceManager.cs
+++
@@ -1,9 +0,0 @@
-// This code is licensed under MIT license (see LICENSE for details)
-
-namespace Cosmos.Kernel.HAL.BlockDevice;
-
-public static class BlockDeviceManager
-{
-    public static List<BlockDevice> Devices = new List<BlockDevice>();
-    public static List<Partition> Partitions = new List<Partition>();
-}
