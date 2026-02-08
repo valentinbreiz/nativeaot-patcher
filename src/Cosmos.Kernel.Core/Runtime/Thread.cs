f6ca2a3f5011519976a846d445437512c7a2a011
Guillermo Santos <santosguillermo360@outlook.com>
2026-02-08 18:26:19 +0000

    Handle case where Scheduler support is disabled.

--- src/Cosmos.Kernel.Core/Runtime/Thread.cs
+++ src/Cosmos.Kernel.Core/Runtime/Thread.cs
@@ -5,10 +5,20 @@ namespace Cosmos.Kernel.Core.Runtime;
 
 public class Thread
 {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    private static object[][] threadData;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     [RuntimeExport("RhGetThreadStaticStorage")]
     static ref object[][] RhGetThreadStaticStorage()
     {
-        var cpuState = SchedulerManager.GetCpuState(0);
-        return ref cpuState.CurrentThread!.GetThreadStaticStorage();
+        if (CosmosFeatures.SchedulerEnabled)
+        {
+            var cpuState = SchedulerManager.GetCpuState(0);
+            return ref cpuState.CurrentThread!.GetThreadStaticStorage();
+        }
+        else
+        {
+            return ref threadData;
+        }
     }
 }
