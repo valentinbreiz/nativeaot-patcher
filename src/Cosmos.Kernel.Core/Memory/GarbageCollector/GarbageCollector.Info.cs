e8ff5799260b13ae7166ff06bf0ccc0c7d38b378
Valentin Arthur Thomas <64769541+warquys@users.noreply.github.com>
2026-04-02 17:59:56 +0200

    Add GC Variables

--- src/Cosmos.Kernel.Core/Memory/GarbageCollector/GarbageCollector.Info.cs
+++ src/Cosmos.Kernel.Core/Memory/GarbageCollector/GarbageCollector.Info.cs
@@ -27,6 +27,94 @@ public static unsafe partial class GarbageCollector
         public int CondemnedGeneration;
     }
 
+    // staticly define for now
+    static Dictionary<string, object>? variables = null;
+    public static IReadOnlyDictionary<string, object> Variables
+    {
+        get
+        {
+            if (!s_initialized)
+            {
+                return null!; // GC config variables are available only after GC initialization
+            }
+
+            if (variables == null)
+            {
+                variables = new Dictionary<string, object>()
+                {
+                    // "Whether we should be using Server GC"
+                    ["gcServer"] = false,
+                    // Whether we should be using Concurrent GC
+                    ["gcConcurrent"] = false,
+                    // When set we put the segments that should be deleted on a standby list (instead of
+                    // releasing them back to the OS) which will be considered to satisfy new segment requests
+                    // (note that the same thing can be specified via API which is the supported way)
+                    ["GCRetainVM"] = false,
+                    // If set, do not affinitize server GC threads
+                    ["GCNoAffinitize"] = false,
+                    // Enables CPU groups in the GC
+                    ["GCCpuGroup"] = false,
+                    // Enables using Large Pages in the GC
+                    ["GCLargePages"] = false,
+                    // Specifies the size that will make objects go on LOH
+                    //["GCLOHThreshold"] = 0,
+                    // Specifies the number of server GC heaps
+                    //["GCHeapCount"] = 0,
+                    // Specifies the max number of server GC heaps to adjust to
+                    //["GCMaxHeapCount"] = 0,
+                    // Specifies processor mask for Server GC threads
+                    //["GCHeapAffinitizeMask"] = 0,
+                    // Specifies list of processors for Server GC threads. The format is a comma separated list of
+                    // processor numbers or ranges of processor numbers. Format need to be specified in the doc.
+                    //["GCHeapAffinitizeRanges"] = "",
+                    // The percent for GC to consider as high memory
+                    //["GCHighMemPercent"] = 0,
+                    // Specifies the largest gen0 allocation budget
+                    //["GCGen0MaxBudget"] = 0,
+                    // Specifies a hard limit for the GC heap
+                    //["GCHeapHardLimit"] = 0,
+                    // Specifies the GC heap usage as a percentage of the total memory
+                    //["GCHeapHardLimitPercent"] = 0,
+                    // Specifies the range for the GC heap
+                    //["GCRegionRange"] = 0,
+                    // Specifies the size for a basic GC region
+                    //["GCRegionSize"] = 0,
+                    // UOH allocation during a BGC waits till end of BGC after UOH increases by this percent
+                    //["UOHWaitBGCSizeIncPercent"] = -1,
+                    // Specifies a hard limit for the GC heap SOH
+                    //["GCHeapHardLimitSOH"] = 0,
+                    // Specifies a hard limit for the GC heap LOH
+                    //["GCHeapHardLimitLOH"] = 0,
+                    // Specifies a hard limit for the GC heap POH
+                    //["GCHeapHardLimitPOH"] = 0,
+                    // Specifies the GC heap SOH usage as a percentage of the total memory
+                    //["GCHeapHardLimitSOHPercent"] = 0,
+                    // Specifies the GC heap LOH usage as a percentage of the total memory
+                    //["GCHeapHardLimitLOHPercent"] = 0,
+                    // Specifies the GC heap POH usage as a percentage of the total memory
+                    //["GCHeapHardLimitPOHPercent"] = 0,
+                    // Specifies how hard GC should try to conserve memory - values 0-9
+                    //["GCConserveMemory"] = 0,
+                    // Specifies the name of the standalone GC implementation.
+                    ["GCName"] = "OrionGC",
+                    // Specifies the path of the standalone GC implementation.
+                    ["GCPath"] = "",
+                    // Enable the GC to dynamically adapt to application sizes.
+                    //["GCDynamicAdaptationMode"] = 1,
+                    // Specifies the target tcp for DATAS
+                    //["GCDTargetTCP"] = 0,
+                    // Specifies the percentage of the default growth factor
+                    //["GCDGen0GrowthPercent"] = 0,
+                    // Specifies the minimum growth factor in permil
+                    //["GCDGen0GrowthMinFactor"] = 0,
+                    // Specifies the maximum growth factor in permil
+                    //["GCDGen0GrowthMaxFactor"] = 0,
+                };
+            }
+            return variables!;
+        }
+    }
+
     public static ulong GetLastGenSizeBefore(int gen)
     {
         if (gen != 0)
