00340c49e50fa55c49d4c5f69162130415d0e36e
Guillermo Santos <96314559+Guillermo-Santos@users.noreply.github.com>
2026-04-02 13:23:30 -0400

    Add some missing runtime exports (#295)
    * Add some runtime exports
    
    * Add runtime exports for RhGetGeneration and ModF function
    
    * Fix RhGetGCDescSize

--- src/Cosmos.Kernel.Core/Runtime/MetaTable.cs
+++ src/Cosmos.Kernel.Core/Runtime/MetaTable.cs
@@ -1,6 +1,8 @@
 using System.Runtime;
 using System.Runtime.CompilerServices;
+using Cosmos.Kernel.Core.IO;
 using Cosmos.Kernel.Core.Memory.GarbageCollector;
+using Internal.Runtime;
 
 namespace Cosmos.Kernel.Core.Runtime;
 
@@ -37,4 +39,15 @@ public unsafe class MetaTable
     {
         GarbageCollector.HandleSetSecondary(handle, pSecondary);
     }
+
+    [RuntimeExport("RhGetRuntimeHelperForType")]
+    internal static unsafe IntPtr RhGetRuntimeHelperForType(MethodTable* pEEType, RuntimeHelperKind kind)
+    {
+        return __RhGetRuntimeHelperForType(null!, (IntPtr)pEEType, kind);
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "RhGetRuntimeHelperForType")]
+    private static extern IntPtr __RhGetRuntimeHelperForType([UnsafeAccessorType("System.Runtime.RuntimeExports")] object @this
+            , [UnsafeAccessorType("Internal.Runtime.MethodTable*")] object pEEType
+            , [UnsafeAccessorType("Internal.Runtime.RuntimeHelperKind")] object kind);
 }
