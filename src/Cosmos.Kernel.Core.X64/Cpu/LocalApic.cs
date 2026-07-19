2d536b317961467abeb8e8ca999e95d9c0073851
valentinbreiz <valentinbreiz@gmail.com>
2026-07-19 22:17:36 +0200

    💚 Add missing Cosmos.Kernel.Core.Memory using in LocalApic

--- src/Cosmos.Kernel.Core.X64/Cpu/LocalApic.cs
+++ src/Cosmos.Kernel.Core.X64/Cpu/LocalApic.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 using Cosmos.Kernel.Core;
 using Cosmos.Kernel.Core.CPU;
 using Cosmos.Kernel.Core.IO;
+using Cosmos.Kernel.Core.Memory;
 using Cosmos.Kernel.Core.Scheduler;
 
 namespace Cosmos.Kernel.Core.X64.Cpu;
